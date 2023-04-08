de209acc457cf7784e3c7011279cbd188f889668
pbeheyt <pbeheyt@student.42.fr>
2023-04-08 14:09:57 +0200

    ex00 makefile done + start main

--- Module_00/ex00/megaphone.cpp
+++ Module_00/ex00/megaphone.cpp
@@ -6,7 +6,16 @@
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 12:27:22 by pbeheyt           #+#    #+#             */
-/*   Updated: 2023/04/08 12:27:23 by pbeheyt          ###   ########.fr       */
+/*   Updated: 2023/04/08 14:09:30 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    (void)ac;
+    (void)av;
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
