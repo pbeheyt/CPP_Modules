b53057424d170134d899f191a683a5aca34e7a86
pbeheyt <pbeheyt@student.42.fr>
2023-04-13 22:40:06 +0200

    ex04 ok

---
+++ Module_01/ex04/main.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 20:10:55 by pbeheyt           #+#    #+#             */
+/*   Updated: 2023/04/13 22:38:58 by pbeheyt          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+
+std::string convert(const std::string& line, const std::string& s1, const std::string& s2)
+{
+    std::string convLine;
+    size_t i = 0;
+    int len1 = s1.length();
+
+    while (i < line.length()) {
+        if (line.compare(i, len1, s1) == 0) {
+            convLine += s2;
+            i += len1;
+        } else {
+            convLine.push_back(line[i]);
+            i++;
+        }
+    }
+    return convLine;
+}
+
+int	main(int ac, char **av)
+{
+	std::string	line;
+	
+	if (ac != 4)
+		return (std::cout << "Error\nInvalid number of arguments" << std::endl, -1);
+	std::string file = av[1];
+	std::ifstream ifs(file.c_str());
+	if(!ifs)
+		return (std::cout << "Error\nCan't open file" << std::endl, -1);
+	std::string s1 = av[2];
+	std::string s2 = av[3];
+	file += ".replace";
+	std::ofstream ofs(file.c_str());
+	while (ifs.good()) {
+		getline(ifs, line);
+		ofs << convert(line, s1, s2);
+		if (ifs.peek() != EOF)
+			ofs << std::endl;
+	}
+	ifs.close();
+	return 0;
+}
