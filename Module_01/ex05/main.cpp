16ddfc8b1a6030b4262a16ec073537f31f4bce8c
pbeheyt <pbeheyt@student.42.fr>
2023-04-13 23:13:24 +0200

    ex05 ok

---
+++ Module_01/ex05/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 23:06:31 by pbeheyt           #+#    #+#             */
+/*   Updated: 2023/04/13 23:12:28 by pbeheyt          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main(void)
+{
+	Harl harl;
+	
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("TEST");
+	return (0);
+}
