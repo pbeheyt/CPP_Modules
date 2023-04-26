b6847490c66ccda8f69e34d6b9a5c81c84277390
Pierre BEHEYT <pbeheyt@e2r1p10.clusters.42paris.fr>
2023-04-26 04:13:13 +0200

    ex04 tests OK

--- Module_01/ex04/main.cpp
+++ Module_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 20:10:55 by pbeheyt           #+#    #+#             */
-/*   Updated: 2023/04/25 03:25:16 by pbeheyt          ###   ########.fr       */
+/*   Updated: 2023/04/26 03:56:19 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ const std::string& s2) {
     int len1 = s1.length();
 
     while (i < line.length()) {
-        if (line.compare(i, len1, s1) == 0) {
+        if (!line.compare(i, len1, s1)) {
             convLine += s2;
             i += len1;
         } else {
@@ -36,21 +36,26 @@ int	main(int ac, char **av) {
 
 	if (ac != 4)
 		return (std::cout << "Error\nInvalid number of arguments" << std::endl, -1);
+		
 	std::string file = av[1];
 	std::ifstream ifs(file.c_str());
 	if(!ifs)
 		return (std::cout << "Error\nCan't open file" << std::endl, -1);
+	
 	std::string s1 = av[2];
 	if (s1.empty())
 		return (std::cout << "Error\nEmpty target argument" << std::endl, -1);
+	
 	std::string s2 = av[3];
 	file += ".replace";
 	std::ofstream ofs(file.c_str());
+	
 	while (ifs.good()) {
 		getline(ifs, line);
 		ofs << convert(line, s1, s2);
-		if (ifs.peek() != EOF)
+		if (ifs.peek() != EOF) {
 			ofs << std::endl;
+		}
 	}
 	return 0;
 }
