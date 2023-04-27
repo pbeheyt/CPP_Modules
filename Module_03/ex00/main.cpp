6048ea50b85546b8a4d22d1cabda0e9ccdb13855
Pierre BEHEYT <pbeheyt@e2r1p10.clusters.42paris.fr>
2023-04-27 23:44:33 +0200

    maj main

--- Module_03/ex00/main.cpp
+++ Module_03/ex00/main.cpp
@@ -6,30 +6,30 @@
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 04:31:24 by pbeheyt           #+#    #+#             */
-/*   Updated: 2023/04/27 23:09:35 by pbeheyt          ###   ########.fr       */
+/*   Updated: 2023/04/27 23:43:52 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
 int main(void) {
-    ClapTrap clap("Sylvain");
-	ClapTrap clap2("Michel");
-	ClapTrap clap3(clap);
-	ClapTrap clap4;
-	clap4.operator=(clap2);
+    ClapTrap trap("Sylvain");
+	ClapTrap trap2("Michel");
+	ClapTrap trap3(trap);
+	ClapTrap trap4;
+	trap4.operator=(trap2);
 	std::cout << std::endl;
 
-    clap.printStatus();
-    clap.attack("enemy");
-    clap.takeDamage(5);
-    clap.beRepaired(3);
+    trap.printStatus();
+    trap.attack("enemy");
+    trap.takeDamage(5);
+    trap.beRepaired(3);
 	std::cout << std::endl;
 
-    clap2.printStatus();
-	clap2.attack("enemy");
-    clap2.takeDamage(10);
-    clap2.beRepaired(2);
+    trap2.printStatus();
+	trap2.attack("enemy");
+    trap2.takeDamage(10);
+    trap2.beRepaired(2);
 	std::cout << std::endl;
 
     return 0;
