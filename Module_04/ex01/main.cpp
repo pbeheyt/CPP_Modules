664546cfdcc92f2e8cb7ae376dff033b3aa80fbc
Pierre BEHEYT <pbeheyt@e2r3p2.clusters.42paris.fr>
2023-04-27 05:39:06 +0200

    ex01 ok

--- Module_04/ex01/main.cpp
+++ Module_04/ex01/main.cpp
@@ -6,75 +6,43 @@
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 07:08:41 by pbeheyt           #+#    #+#             */
-/*   Updated: 2023/04/27 04:50:59 by pbeheyt          ###   ########.fr       */
+/*   Updated: 2023/04/27 05:37:59 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 #include "Dog.hpp"
-#include "WrongCat.hpp"
 
 int main(void)
 {
-	// const Animal* meta = new Animal();
-	// const Animal* j = new Dog();
-	// const Animal* i = new Cat();
-	// std::cout << j->getType() << " " << std::endl;
-	// std::cout << i->getType() << " " << std::endl;
-	// i->makeSound(); //will output the cat sound!
-	// j->makeSound();
-	// meta->makeSound();
-	// delete meta;
-	// delete j;
-	// delete i;
-	// std::cout	<< std::endl
-	// 			<< std::endl
-	// 			<< std::endl;
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	delete j;//should not create a leak
+	delete i;
+	std::cout << std::endl;
 
-	// const WrongAnimal* metatest = new WrongAnimal();
-	// const WrongAnimal* x = new WrongCat();
-	// std::cout << x->getType() << " " << std::endl;
-	// x->makeSound(); //will output the cat sound!
-	// metatest->makeSound();
-	// delete metatest;
-	// delete x;
-	// std::cout << std::endl;
+	Animal	*animalList[10];
+	
+	for (int i = 0; i < 10 ; i++) {
+		if (i % 2 == 0) {
+			animalList[i] = new Cat();
+		} else {
+			animalList[i] = new Dog();
+		}
+	}
+	std::cout	<< std::endl;
 
-
-    // // Test Animal
-    // const Animal* a = new Animal();
-    // std::cout << a->getType() << std::endl;
-    // a->makeSound();
-    // delete a;
-	// std::cout << std::endl;
-
-    // // Test Dog
-    // const Animal* b = new Dog();
-    // std::cout << b->getType() << std::endl;
-    // b->makeSound();
-    // delete b;
-	// std::cout << std::endl;
-
-    // // Test Cat
-    // const Animal* c = new Cat();
-    // std::cout << c->getType() << std::endl;
-    // c->makeSound();
-    // delete c;
-	// std::cout << std::endl;
-
-    // // Test WrongAnimal
-    // const WrongAnimal* wa = new WrongAnimal();
-    // std::cout << wa->getType() << std::endl;
-    // wa->makeSound();
-    // delete wa;
-	// std::cout << std::endl;
-
-    // // Test WrongCat
-    // const WrongAnimal* wc = new WrongCat();
-    // std::cout << wc->getType() << std::endl;
-    // wc->makeSound();
-    // delete wc;
-	// std::cout << std::endl;
+	for (int i = 0; i < 10 ; i++) {
+		std::cout	<< "Random " << animalList[i]->getType() << ": " << std::flush;
+		animalList[i]->makeSound();
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	
+	for (int i = 0; i < 10 ; i++) {
+		delete animalList[i];
+	}
+	std::cout << std::endl;
 
     return 0;
 }
