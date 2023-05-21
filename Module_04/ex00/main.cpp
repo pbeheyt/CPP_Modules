74564cd2a04e3e7cd76b2631e033e10d145091dc
Pierre BEHEYT <pbeheyt@e3r2p23.clusters.42paris.fr>
2023-05-21 06:49:28 +0200

    tests ok

--- Module_04/ex00/main.cpp
+++ Module_04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 07:08:41 by pbeheyt           #+#    #+#             */
-/*   Updated: 2023/04/27 04:25:13 by pbeheyt          ###   ########.fr       */
+/*   Updated: 2023/05/21 06:48:47 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,18 @@
 int main(void)
 {
 	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
 	const Animal* i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
+	const Animal* j = new Dog();
 	std::cout << i->getType() << " " << std::endl;
+	std::cout << j->getType() << " " << std::endl;
 	i->makeSound(); //will output the cat sound!
 	j->makeSound();
 	meta->makeSound();
 	delete meta;
-	delete j;
 	delete i;
+	delete j;
 	std::cout	<< std::endl
 				<< std::endl
-				<< std::endl;
 
 	const WrongAnimal* metatest = new WrongAnimal();
 	const WrongAnimal* x = new WrongCat();
