1378deb0564cecb83f829d69a84a0002f7dfc6ea
Pierre BEHEYT <pbeheyt@e2r1p3.clusters.42paris.fr>
2023-05-25 02:41:11 +0200

    add const ref to character constr

--- Module_04/ex03/Character.cpp
+++ Module_04/ex03/Character.cpp
@@ -6,13 +6,13 @@
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 23:24:09 by pbeheyt           #+#    #+#             */
-/*   Updated: 2023/05/24 23:25:02 by pbeheyt          ###   ########.fr       */
+/*   Updated: 2023/05/25 02:40:17 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Character.hpp"
 
-Character::Character(std::string name) : _name(name) {
+Character::Character(std::string const &name) : _name(name) {
 	std::cout << "[ Character ] - Parameterized constructor called" << std::endl;
     for (int i = 0; i < 4; i++) {
         this->_inventory[i] = NULL;
--- Module_04/ex03/Character.hpp
+++ Module_04/ex03/Character.hpp
@@ -6,7 +6,7 @@
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 23:24:09 by pbeheyt           #+#    #+#             */
-/*   Updated: 2023/05/21 05:32:20 by pbeheyt          ###   ########.fr       */
+/*   Updated: 2023/05/25 02:40:05 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 class Character : public ICharacter {
 	public:
-		Character(std::string name);
+		Character(std::string const &name);
 		Character(Character const &rhs);
 		Character &operator=(Character const &rhs);
 		~Character(void);
