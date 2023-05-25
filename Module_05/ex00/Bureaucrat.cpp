6d37743fa11d82c51ac17843a8c6079b74b8dbb3
Pierre BEHEYT <pbeheyt@e2r1p3.clusters.42paris.fr>
2023-05-25 07:42:01 +0200

    first functions of .cpp w/o exception handling

--- Module_05/ex00/Bureaucrat.cpp
+++ Module_05/ex00/Bureaucrat.cpp
@@ -6,21 +6,27 @@
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 07:01:14 by pbeheyt           #+#    #+#             */
-/*   Updated: 2023/05/25 07:29:05 by pbeheyt          ###   ########.fr       */
+/*   Updated: 2023/05/25 07:40:51 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat(void) {
+Bureaucrat::Bureaucrat(void) :
+	_name("Unnamed"), _grade(150) {
 	
 }
-Bureaucrat::Bureaucrat(Bureaucrat const &rhs) {
+Bureaucrat::Bureaucrat(Bureaucrat const &rhs) :
+	_name(rhs._name), _grade(rhs._grade) {
 	
 }
 
 Bureaucrat &Bureaucrat::operator=(Bureaucrat const &rhs) {
-	
+	if (this != &rhs) {
+		// this->_name = rhs._name;
+		this->_grade = rhs._grade;
+	}
+	return *this;
 }
 
 Bureaucrat::~Bureaucrat(void) {
@@ -29,20 +35,24 @@ Bureaucrat::~Bureaucrat(void) {
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 
-std::string const	&getName(void) {
-	
+std::string const	&Bureaucrat::getName(void) {
+	return this->_name;
 }
 
-int	getGrade(void) {
-	
+int	Bureaucrat::getGrade(void) {
+	return this->_grade;
 }
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 
-void	incrementGrade(void) {
-	
+void	Bureaucrat::incrementGrade(void) {
+	if (this->_grade > 1) {
+		this->_grade--;
+	}
 }
 
-void	decrementGrade(void) {
-	
+void	Bureaucrat::decrementGrade(void) {
+	if (this->_grade < 150) {
+		this->_grade++;
+	}
 }
