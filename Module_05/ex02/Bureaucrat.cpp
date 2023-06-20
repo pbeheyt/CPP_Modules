033ffaa66c4918dee38eb384f38466c20ff50039
Pierre BEHEYT <pbeheyt@bess-f4r1s14.clusters.42paris.fr>
2023-06-20 05:50:47 +0200

    add previous ex file

---
+++ Module_05/ex02/Bureaucrat.cpp
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/25 07:01:14 by pbeheyt           #+#    #+#             */
+/*   Updated: 2023/05/25 09:31:54 by pbeheyt          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+Bureaucrat::Bureaucrat(void) : _name("Unnamed"), _grade(150) {}
+
+Bureaucrat::Bureaucrat(std::string const name, int grade) : _name(name) {
+	if (grade > 150) {
+		throw Bureaucrat::GradeTooLowException();
+	} else if (grade < 1) {
+		throw Bureaucrat::GradeTooHighException();
+	} else {
+		this->_grade = grade;
+	}
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &rhs) :
+	_name(rhs._name), _grade(rhs._grade) {}
+
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &rhs) {
+	if (this != &rhs) {
+		this->_grade = rhs._grade;
+	}
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat(void) {}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+std::string const	&Bureaucrat::getName(void) const {
+	return this->_name;
+}
+
+int	Bureaucrat::getGrade(void) const {
+	return this->_grade;
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+void	Bureaucrat::incrementGrade(void) {
+	if (this->_grade <= 1) {
+		throw Bureaucrat::GradeTooHighException();
+	} else {
+		this->_grade--;
+	}
+}
+
+void	Bureaucrat::decrementGrade(void) {
+	if (this->_grade >= 150) {
+		throw Bureaucrat::GradeTooLowException();
+	} else {
+		this->_grade++;
+	}
+}
+
+void	Bureaucrat::signForm(Form &form) const {
+	try {
+		form.beSigned(*this);
+		std::cout << *this << " signed " << form << std::endl;
+	}
+	catch(const std::exception& e) {
+		std::cerr << *this << " couldn’t sign " << form << " because " << e.what() << std::endl;
+	}
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+std::ostream &operator<<(std::ostream &ofs, Bureaucrat const &rhs) {
+	ofs << rhs.getName() << ", bureaucrat grade " << rhs.getGrade();
+	return ofs;
+}
---
+++ Module_05/ex02/Bureaucrat.hpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/25 07:01:11 by pbeheyt           #+#    #+#             */
+/*   Updated: 2023/06/20 05:50:03 by pbeheyt          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MODULE_05_EX02_BUREAUCRATE_HPP_
+#define MODULE_05_EX02_BUREAUCRATE_HPP_
+
+#include <iostream>
+
+class Form;
+
+class Bureaucrat {
+	public:
+		Bureaucrat(void);
+		Bureaucrat(std::string const name, int grade);
+		Bureaucrat(Bureaucrat const &rhs);
+		Bureaucrat &operator=(Bureaucrat const &rhs);
+		~Bureaucrat(void);
+
+		std::string const	&getName(void) const;
+		int					getGrade(void) const;
+		
+		void	incrementGrade(void);
+		void	decrementGrade(void);
+		void	signForm(Form &form) const;
+
+		class GradeTooHighException : public std::exception {
+			public:
+				char const *what() const throw() {
+					return "the grade is too high!";
+				}
+		};
+
+		class GradeTooLowException : public std::exception {
+			public:
+				char const *what() const throw() {
+					return "the grade is too low!";
+				}
+		};
+
+	private:
+		std::string const	_name;
+		int					_grade;
+};
+
+std::ostream &operator<<(std::ostream &ofs, Bureaucrat const &rhs);
+
+#endif // MODULE_05_EX02_BUREAUCRATE_HPP_
