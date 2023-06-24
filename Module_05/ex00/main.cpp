32860e6c3c0c23ec2244ccea2927f8c658a3ab00
pbeheyt <pbeheyt@student.42.fr>
2023-06-24 08:04:54 +0200

    tests ok

--- Module_05/ex00/main.cpp
+++ Module_05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 06:59:49 by pbeheyt           #+#    #+#             */
-/*   Updated: 2023/05/25 08:12:04 by pbeheyt          ###   ########.fr       */
+/*   Updated: 2023/06/24 08:01:35 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	main(void) {
     try {
-        Bureaucrat bureaucrat1("John Doe", 100);
-        Bureaucrat bureaucrat2("Jane Smith", 50);
+        Bureaucrat bureaucrat1("Sylvain", 100);
+        Bureaucrat bureaucrat2("Jean", 50);
 
         std::cout << bureaucrat1 << std::endl;
         std::cout << bureaucrat2 << std::endl;
@@ -34,11 +34,11 @@ int	main(void) {
         std::cout << bureaucrat2 << std::endl;
 		std::cout << std::endl;
 
-        Bureaucrat bureaucrat3("Invalid", 200);
+        Bureaucrat bureaucrat3("Error Low", 200);
 		std::cout << std::endl;
 		
     } catch (std::exception const &e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
+        std::cerr << "Error: " << e.what() << std::endl;
     }
     return 0;
 }
