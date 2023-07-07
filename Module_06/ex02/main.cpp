9f803a9231cea97da8ecd067899215c840883840
pbeheyt <pbeheyt@student.42.fr>
2023-07-07 18:57:58 +0200

    syntax

--- Module_06/ex02/main.cpp
+++ Module_06/ex02/main.cpp
@@ -6,15 +6,13 @@
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 11:55:12 by pbeheyt           #+#    #+#             */
-/*   Updated: 2023/07/07 18:52:34 by pbeheyt          ###   ########.fr       */
+/*   Updated: 2023/07/07 18:57:47 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
 
 int main(void) {
-
-
 	Base base;
     
 	Base *ptr = base.generate();
