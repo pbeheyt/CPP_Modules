97a4029e2c79aaa8d9aaa8e75a73b49f4f57d388
pbeheyt <pbeheyt@student.42.fr>
2023-07-07 18:28:37 +0200

    update main output

--- Module_06/ex01/main.cpp
+++ Module_06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 10:36:23 by pbeheyt           #+#    #+#             */
-/*   Updated: 2023/07/07 18:16:11 by pbeheyt          ###   ########.fr       */
+/*   Updated: 2023/07/07 18:27:55 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,12 @@
 int main(void) {
     Data data;
     data.nb = 42;
-    data.raw = "Hello, World!";
+    data.raw = "123test";
+
+    std::cout << "Data:" << std::endl;
+    std::cout << "nb: " << data.nb << std::endl;
+    std::cout << "raw: " << data.raw << std::endl;
+    std::cout << std::endl;
 
     Serializer serializer;
 
