770bf0307e30acfd42eaaa0723a91e0d1a08fbfd
pbeheyt <pbeheyt@student.42.fr>
2023-07-08 16:17:17 +0200

    final tests ok

--- Module_07/ex01/iter.hpp
+++ Module_07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 08:08:07 by pbeheyt           #+#    #+#             */
-/*   Updated: 2023/07/04 09:48:59 by pbeheyt          ###   ########.fr       */
+/*   Updated: 2023/07/08 15:37:56 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,25 @@
 #include <iostream>
 
 template <typename T>
-void iter(T *array, int len, void (*f)(T const &)) {
-	for (int i = 0; i < len; i++) {
+void iter(T *array, size_t size, void (*f)(T const &)) {
+	for (size_t i = 0; i < size; i++) {
 		(*f)(array[i]);
 	}
 }
 
 template <typename T>
 void printElement(T const &e) {
-    std::cout << e << " ";
+    std::cout << e << std::endl;
 }
 
 template <typename T>
 void printSquareElement(T const &e) {
-    std::cout << e * e << " ";
+    std::cout << e * e << std::endl;
 }
 
 template <typename T>
 void printIncrementElement(T const &e) {
-    std::cout << e + 1 << " ";
+    std::cout << e + 1 << std::endl;
 }
 
 #endif  // MODULE_07_EX01_ITER_HPP_
--- Module_07/ex01/main.cpp
+++ Module_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 08:12:00 by pbeheyt           #+#    #+#             */
-/*   Updated: 2023/07/04 08:31:08 by pbeheyt          ###   ########.fr       */
+/*   Updated: 2023/07/08 16:16:01 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,25 @@ int main(void) {
     char charArray[] = {'a', 'b', 'c', 'd', 'e'};
 
     // Testing iter() with int array
-    std::cout << "Iterating over intArray: ";
+    std::cout << "Iterating over intArray: " << std::endl;;
     iter(intArray, 5, printElement<int>);
     std::cout << std::endl;
 
+    std::cout << "Square and print over intArray: " << std::endl;;
 	iter(intArray, 5, printSquareElement<int>);
     std::cout << std::endl;
 
     // Testing iter() with double array
-    std::cout << "Iterating over doubleArray: ";
+    std::cout << "Iterating over doubleArray: " << std::endl;;
     iter(doubleArray, 5, printElement<double>);
     std::cout << std::endl;
 
+    std::cout << "Increment and print over doubleArray: " << std::endl;;
     iter(doubleArray, 5, printIncrementElement<double>);
     std::cout << std::endl;
 
     // Testing iter() with char array
-    std::cout << "Iterating over charArray: ";
+    std::cout << "Iterating over charArray: " << std::endl;;
     iter(charArray, 5, printElement<char>);
     std::cout << std::endl;
 
