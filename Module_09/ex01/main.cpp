893b6d91c69f35bc148558819f29df0ce6dd1689
Pierre BEHEYT <pbeheyt@made-f0Br8s8.clusters.42paris.fr>
2023-09-07 22:54:09 +0200

    syntax

--- Module_09/ex01/main.cpp
+++ Module_09/ex01/main.cpp
@@ -6,21 +6,21 @@
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 03:38:37 by pbeheyt           #+#    #+#             */
-/*   Updated: 2023/09/01 03:41:14 by pbeheyt          ###   ########.fr       */
+/*   Updated: 2023/09/07 22:50:39 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <expression>" << std::endl;
+int main(int ac, char** av) {
+    if (ac != 2) {
+        std::cerr << "Usage: " << av[0] << " <expression>" << std::endl;
         return 1;
     }
 
     RPNCalculator calculator;
-    double result = calculator.evaluate(argv[1]);
+    double result = calculator.evaluate(av[1]);
     std::cout << "Result: " << result << std::endl;
 
     return 0;
