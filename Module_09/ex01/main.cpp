8e1077c673e203daca7c815b4488c4c53ce50cfd
Pierre BEHEYT <pbeheyt@made-f0Cr6r6.clusters.42paris.fr>
2023-09-08 22:06:46 +0200

    syntax

--- Module_09/ex01/main.cpp
+++ Module_09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 03:38:37 by pbeheyt           #+#    #+#             */
-/*   Updated: 2023/09/08 21:50:47 by pbeheyt          ###   ########.fr       */
+/*   Updated: 2023/09/08 22:06:28 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,10 @@
 int main(int ac, char** av) {
     if (ac != 2) {
         std::cerr << "Usage: " << av[0] << " \"<expression>\"" << std::endl;
-        return 1;
-    }
-
-    RPNCalculator calculator;
-    double result = calculator.evaluate(av[1]);
-    std::cout << "Result: " << result << std::endl;
-
+    } else {
+		RPNCalculator calculator;
+		double result = calculator.evaluate(av[1]);
+		std::cout << "Result: " << result << std::endl;
+	}
     return 0;
 }
